7272b87aca8bd36e1d1f437263f1540846b684e5
Jon Jagger <jon@jaggersoft.com>
2020-04-19 09:44:38 +0100

    Add circleci files

---
+++ .circleci/deploy_to_namespace.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -Eeu
+
+# Normally I export the cyberdojo env-vars using the command
+# $ docker run --rm cyberdojo/versioner:latest
+# This won't work on the.circleci deployment step since it is
+# run inside the cyberdojo/gcloud-kubectl-helm image which does
+# not have docker. So doing it directly from versioner's git repo
+export $(curl https://raw.githubusercontent.com/cyber-dojo/versioner/master/app/.env)
+
+readonly NAMESPACE="${1}" # eg beta
+readonly IMAGE="${CYBER_DOJO_SHAS_IMAGE}"
+readonly PORT="${CYBER_DOJO_SHAS_PORT}"
+readonly TAG="${CIRCLE_SHA1:0:7}"
+
+# misc env-vars are in ci context
+
+echo ${GCP_K8S_CREDENTIALS} > /gcp/gcp-credentials.json
+
+gcloud auth activate-service-account \
+  "${SERVICE_ACCOUNT}" \
+  --key-file=/gcp/gcp-credentials.json
+
+gcloud container clusters get-credentials \
+  "${CLUSTER}" \
+  --zone "${ZONE}" \
+  --project "${PROJECT}"
+
+helm init --client-only
+
+helm repo add praqma https://praqma-helm-repo.s3.amazonaws.com/
+
+helm upgrade \
+  --install \
+  --namespace=${NAMESPACE} \
+  --set-string containers[0].image=${IMAGE} \
+  --set-string containers[0].tag=${TAG} \
+  --set service.port=${PORT} \
+  --set containers[0].livenessProbe.port=${PORT} \
+  --set containers[0].readinessProbe.port=${PORT} \
+  --set-string service.annotations."prometheus\.io/port"=${PORT} \
+  --values .circleci/shas-values.yaml \
+  ${NAMESPACE}-shas \
+  praqma/cyber-dojo-service \
+  --version 0.2.4
