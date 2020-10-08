18f6a779b7affd9f09e5f80a87954d0844352d0c
Jon Jagger <jon@jaggersoft.com>
2020-10-08 08:04:46 +0100

    Make Dockerhub pulls non-anonymous on CI

--- sh/on_ci_publish_tagged_images.sh
+++ sh/on_ci_publish_tagged_images.sh
@@ -15,10 +15,7 @@ on_ci_publish_tagged_images()
     echo 'on CI so publishing tagged images'
     local -r name="$(image_name)"
     local -r tag="$(image_tag)"
-    # DOCKER_USER, DOCKER_PASS are in ci context
-    echo "${DOCKER_PASS}" | docker login --username "${DOCKER_USER}" --password-stdin
     docker push "${name}:${tag}"
     docker push "${name}:latest"
-    docker logout
   fi
 }
