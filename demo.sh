7771cb72dac190f162d6aec301619f6958cc2292
Jon Jagger <jon@jaggersoft.com>
2020-10-09 11:09:13 +0100

    Add background image of 8x8 grey avatars

--- demo.sh
+++ demo.sh
@@ -1,17 +1,18 @@
 #!/bin/bash -Eeu
 
-readonly SH_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/sh" && pwd)"
-source "${SH_DIR}/versioner_env_vars.sh"
-export $(versioner_env_vars)
-source "${SH_DIR}/build_images.sh"
+export ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export SH_DIR="${ROOT_DIR}/sh"
+source "${SH_DIR}/build_tagged_images.sh"
 source "${SH_DIR}/containers_down.sh"
 source "${SH_DIR}/containers_up.sh"
 source "${SH_DIR}/ip_address.sh"
+source "${SH_DIR}/versioner_env_vars.sh"
+export $(versioner_env_vars)
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - -
 api_demo()
 {
-  build_images
+  build_tagged_images
   containers_up api-demo
   echo
   demo
