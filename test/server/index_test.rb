0b03ed8b29d63eba82c2d92e25ee06ad95fcd8d7
Jon Jagger <jon@jaggersoft.com>
2020-11-28 10:14:57 +0000

    Updated tests

--- test/server/index_test.rb
+++ test/server/index_test.rb
@@ -28,7 +28,7 @@ class IndexTest < TestBase
 
   SERVICE_NAMES = %w(
     custom-start-points exercises-start-points languages-start-points
-    creator differ runner saver shas
+    creator dashboard differ runner saver shas
   )
 
 end
