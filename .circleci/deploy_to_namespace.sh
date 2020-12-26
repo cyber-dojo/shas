cb82effd8eeae11b5579393fbeb7449d03fc3943
Jon Jagger <jon@jaggersoft.com>
2020-12-26 14:28:06 +0000

    Fix deploy script errors.

--- .circleci/deploy_to_namespace.sh
+++ .circleci/deploy_to_namespace.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -Eeu
 
+readonly MY_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly NAMESPACE="${1}" # beta|prod
 readonly K8S_URL=https://raw.githubusercontent.com/cyber-dojo/k8s-install/master
 readonly VERSIONER_URL=https://raw.githubusercontent.com/cyber-dojo/versioner/master
 source <(curl "${K8S_URL}/sh/deployment_functions.sh")
 export $(curl "${VERSIONER_URL}/app/.env")
 readonly CYBER_DOJO_SHAS_TAG="${CIRCLE_SHA1:0:7}"
+readonly YAML_VALUES_FILE="${MY_DIR}/k8s-general-values.yml"
 
 gcloud_init
 helm_init
@@ -21,4 +23,4 @@ helm_upgrade_probe_yes_prometheus_yes \
    "${CYBER_DOJO_SHAS_IMAGE}" \
    "${CYBER_DOJO_SHAS_TAG}" \
    "${CYBER_DOJO_SHAS_PORT}" \
-   ".circleci/k8s-general-values.yml"
+   "${YAML_VALUES_FILE}"
