41478e8027c8f5053a5f0d78ba7e792df22b401e
Jon Jagger <jon@jaggersoft.com>
2021-08-22 20:32:38 +0100

    Pass merkely-fingerprint across pipeline steps

--- sh/on_ci_publish_tagged_images.sh
+++ sh/on_ci_publish_tagged_images.sh
@@ -10,6 +10,10 @@ on_ci_publish_tagged_images()
     echo 'on CI so publishing tagged images'
     docker push "$(image_name):$(git_commit_tag)"
     docker push "$(image_name):latest"
+    local -r digest=$(docker inspect $(image_name):$(git_commit_tag) | jq --raw-output '.[].RepoDigests[]' | cut -d: -f2)
+    # Make MERKELY_FINGERPRINT available for subsequent CI pipeline steps
+    # See https://circleci.com/docs/2.0/env-vars/#using-parameters-and-bash-environment
+    echo "export MERKELY_FINGERPRINT=sha256://${digest}/$(image_name):$(git_commit_tag)" >> $BASH_ENV
   fi
 }
 
