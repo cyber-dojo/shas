960b58164cc0ce4db52347667ba5985df19f49bb
Jon Jagger <jon@jaggersoft.com>
2021-08-22 21:04:18 +0100

    Fix previous undo; restore pulling docker-image to get digest for Merkely-log-deployment; CircleCI seems to have no way to pass a variable from one job to another

--- .circleci/merkely_log_deployment.sh
+++ .circleci/merkely_log_deployment.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -Eeu
 
-
 # - - - - - - - - - - - - - - - - - - -
 merkely_log_deployment()
 {
@@ -8,12 +7,19 @@ merkely_log_deployment()
   local -r MERKELY_PIPELINE=shas
   local -r MERKELY_ENVIRONMENT="${1}"
   local -r MERKELY_HOST="${2}"
-  # MERKELY_FINGERPRINT is set in sh/on_ci_publish_tagged_images.sh
+
+  # Set CYBER_DOJO_SHAS_IMAGE, CYBER_DOJO_SHAS_TAG
+  local -r VERSIONER_URL=https://raw.githubusercontent.com/cyber-dojo/versioner/master
+  export $(curl "${VERSIONER_URL}/app/.env")
+  local -r CYBER_DOJO_SHAS_TAG="${CIRCLE_SHA1:0:7}"
+
+  # Pull image to ensure merkely_fingerprint() works
+  docker pull ${CYBER_DOJO_SHAS_IMAGE}:${CYBER_DOJO_SHAS_TAG}
 
 	docker run \
     --env MERKELY_COMMAND=log_deployment \
     --env MERKELY_OWNER=${MERKELY_OWNER} \
-    --env MERKELY_PIPELINE=${MERKELY_PIPELINE} \
+    --env MERKELY_PIPELINE=$(merkely_fingerprint) \
     --env MERKELY_FINGERPRINT=${MERKELY_FINGERPRINT} \
     --env MERKELY_DESCRIPTION="Deployed to ${environment} in circleci pipeline" \
     --env MERKELY_ENVIRONMENT="${MERKELY_ENVIRONMENT}" \
@@ -25,3 +31,8 @@ merkely_log_deployment()
       merkely/change:latest
 }
 
+# - - - - - - - - - - - - - - - - - - -
+merkely_fingerprint()
+{
+  echo "docker://${CYBER_DOJO_SHAS_IMAGE}:${CYBER_DOJO_SHAS_TAG}"
+}
