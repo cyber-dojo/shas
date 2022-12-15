0198bd3320b69a472e4f582ee44ca76c687946f9
Jon Jagger <jon@jaggersoft.com>
2022-12-15 08:50:13 +0000

    Switch to root_dir() in sh/echo_versioner_env_vars.sh

--- sh/echo_versioner_env_vars.sh
+++ sh/echo_versioner_env_vars.sh
@@ -22,7 +22,7 @@ echo_versioner_env_vars()
 # - - - - - - - - - - - - - - - - - - - - - - - -
 git_commit_sha()
 {
-  echo "$(cd "${ROOT_DIR}" && git rev-parse HEAD)"
+  echo "$(cd "$(root_dir)" && git rev-parse HEAD)"
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
